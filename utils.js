725eb6065bdb1044998a847f00471d57dcd0a9cb
Prasanna Hegde <prasanna.hegde@maiora.co>
2023-06-14 21:38:59 +0530

    double comment issue fixed

--- utils.js
+++ utils.js
@@ -10,7 +10,8 @@ function cleanPythonFiles(fileUris) {
             const currentLine = lines[i];
             const previousLine = lines[i - 1];
 
-            if (!previousLine.includes('#required') && !previousLine.includes('# required') && currentLine.includes('print(')) {
+            if (!previousLine.includes('#required') && !previousLine.includes('# required') &&
+                    currentLine.includes('print(') && !currentLine.trim().startsWith('#')) {
                 const cleanedLine = currentLine.replace('print(', '# print(');
                 lines[i] = cleanedLine;
             }
@@ -31,7 +32,8 @@ function cleanJSFiles(fileUris) {
             const currentLine = lines[i];
             const previousLine = lines[i - 1];
 
-            if (!previousLine.includes('//required') && !previousLine.includes('// required') && currentLine.includes('console.log')) {
+            if (!previousLine.includes('//required') && !previousLine.includes('// required') &&
+                    currentLine.includes('console.log') && !currentLine.trim().startsWith('//')) {
                 const cleanedLine = currentLine.replace('console.log', '// console.log');
                 lines[i] = cleanedLine;
             }
